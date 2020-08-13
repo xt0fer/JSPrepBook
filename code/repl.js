a8c6e07e6707a7159ba91396478afabe377415e4
Kristofer <kris@youngers.org>
2020-08-13 10:24:16 -0400

    v0.9

--- code/repl.js
+++ code/repl.js
@@ -8,28 +8,54 @@ var editor = ace.edit("editor", {
 
 var consolewin = ace.edit("consolewin");
 consolewin.setOptions({
+    theme: "ace/theme/terminal",
     autoScrollEditorIntoView: true,
     maxLines: 8,
     minLines: 8
 });
 consolewin.renderer.setScrollMargin(10, 10, 10, 10);
 
+var
+    preamble = `
+// pre
+(function () {
+    var logg = [];
+    console.log = function() {
+        for (var k = 0; k < arguments.length; k++) {
+            logg.push(arguments[k]);
+        }
+    }
+//pre
+`;
+
+var postamble = `
+//post
+return logg.join("\\n") + "\\n";
+})();
+
+//post
+`;
+
+
 window.addEventListener('message',
+
     function(e) {
         // Sandboxed iframes which lack the 'allow-same-origin'
         // header have "null" rather than a valid origin. This means you still
         // have to be careful about accepting data via the messaging API you
         // create. Check that source, and validate those inputs!
         var frame = document.getElementById('sandboxed');
-        if (e.origin === "null" && e.source === frame.contentWindow)
-        //alert('Result: ' + e.data);
-            consolewin.setValue('Result ' + e.data);
+        if (e.origin === "null" && e.source === frame.contentWindow) {
+            consolewin.setValue(e.data);
+            consolewin.clearSelection(); // This will remove the highlight over the text
+        }
     });
 
 function evaluate() {
     var frame = document.getElementById('sandboxed');
     // var code = document.getElementById('code').value;
-    var code = editor.getValue();
+    var code = preamble + editor.getValue() + postamble;
+    //console.log(code);
     // Note that we're sending the message to "*", rather than some specific
     // origin. Sandboxed iframes which lack the 'allow-same-origin' header
     // don't have an origin which you can target: you'll have to send to any
@@ -37,5 +63,11 @@ function evaluate() {
     frame.contentWindow.postMessage(code, '*');
 }
 
+function clearConsoleWin() {
+    consolewin.setValue("");
+    consolewin.clearSelection(); // This will remove the highlight over the text
+}
+
 document.getElementById('safe').addEventListener('click', evaluate);
+document.getElementById('clear').addEventListener('click', clearConsoleWin);
 document.getElementById("sandboxed").style.display = "none";
